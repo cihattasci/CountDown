8781447965557479df0ca674234e1f80055e1e9b
Cihat Tasci <cihattasci97@gmail.com>
2023-03-06 14:49:31 +0300

    Initial Commit

---
+++ CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Cihat Tascı on 6.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
