1a720559d1f3ac9fc7ba99bfbb9ce04b813c0786
Cihat Tasci <cihattasci97@gmail.com>
2023-03-09 16:16:12 +0300

    last commit

--- CountDown/ContentView.swift
+++ CountDown/ContentView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State var counter: Int = 200
+    @State var isLoop: Bool = true
+    
+    func stopTimer(){
+        timer.upstream.connect().cancel()
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-            Text("Hello, world!")
+            Text("\(counter) is remaining")
+            Button("Start") {
+                stopTimer()
+            }
+            
+            .onReceive(timer) { time in
+                if counter != 0{
+                    counter -= 1
+                } else{
+                    counter = 0
+                }
+            }
         }
     }
 }
